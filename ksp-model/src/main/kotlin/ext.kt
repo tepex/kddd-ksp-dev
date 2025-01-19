4a0684616fa8b2b01623597c9c768e786b58ce6d
Tepex <tepex@mail.ru>
2025-01-19 06:55:19 +0300

    refactor: createMapper

--- ksp-model/src/main/kotlin/ext.kt
+++ ksp-model/src/main/kotlin/ext.kt
@@ -39,20 +39,13 @@ internal val KDType.Boxed.isPrimitive: Boolean get() =
 
 internal typealias KDTypeSearchResult = Pair<KDType, Boolean>
 
-internal fun List<String>.createMapper(type: CollectionType, isMutable: Boolean, hasNotContainsBoxed: Boolean): String {
-    val isNoTerminal =
-        if (hasNotContainsBoxed) false
-        else if (type == MAP) true
-        else type != SET
-
-    return ("".takeIf { hasNotContainsBoxed } ?: when (type) {
-        MAP -> ".entries.associate { ${this[0]} to ${this[1]} }"
-        else -> ".map { ${this.first()} }"
-    }).let { "$it${type.toCounterpart(isMutable, !isNoTerminal)}" }
-}
-
-private fun CollectionType.toCounterpart(isMutable: Boolean, hasToImmutableTermination: Boolean): String =
-    ".toMutable${originName}()".takeIf { isMutable }
-        ?: ".toSet()".takeIf { this == SET }
-        ?: ".to${originName}()".takeIf { hasToImmutableTermination }
-        ?: ""
+internal fun List<String>.createMapper(type: CollectionType, isMutable: Boolean, hasNotContainsBoxed: Boolean): String = (
+    ".toMutable${type.originName}()".takeIf { isMutable }
+        ?: ".toSet()".takeIf { type == SET }
+        ?: ".to${type.originName}()".takeIf { hasNotContainsBoxed || type == MAP } ?: ""
+    ).let { term -> (
+        "".takeIf { hasNotContainsBoxed }
+            ?: ".map { ${this.first()} }".takeUnless { type == MAP }
+            ?: ".entries.associate { ${this[0]} to ${this[1]} }"
+        ).let { "$it$term" }
+    }
