a3e2d05eebf5d225c1a84dd948e72ca1a9612f00
Kirill Terekhov <tepex79@gmail.com>
2025-09-13 21:12:46 +0300

    feat: embed `k3dm` library

--- docs/samples/sandwich.kt
+++ docs/samples/sandwich.kt
@@ -2,9 +2,10 @@
  * Портирование на Котлин примера из статьи
  * https://github.com/graninas/functional-declarative-design-methodology?tab=readme-ov-file
  *
- * https://pl.kotl.in/ki9TpN0nd
- * @version 1.0.0
+ * https://pl.kotl.in/4fL1FfdSu
+ * @version 1.1.0
  * @author Tepex
+ * VCS: https://github.com/tepex/kddd-ksp-dev/blob/examples/docs/samples/sandwich.kt
  */
 
 // ***************************************************
@@ -13,6 +14,7 @@
 
 // -------------------
 // Модуль `api`. eDSL
+// -------------------
 
 /** Ресурсы */
 sealed interface Ingredient {
@@ -25,6 +27,19 @@ sealed interface Ingredient {
     }
 }
 
+interface IngredientsStorage {
+    operator fun get(ingredient: Ingredient): Result<Ingredient>
+}
+
+sealed class IngredientsStorageError(val ingredient: Ingredient, msg: String) : RuntimeException(msg) {
+    class NotFound(
+        ingredient: Ingredient
+    ) : IngredientsStorageError(ingredient, "Ingredient $ingredient not found!")
+}
+
+fun notFound(ingredient: Ingredient) =
+    IngredientsStorageError.NotFound(ingredient)
+
 /** Объект предметной области. ADT, Monad */
 sealed interface Sandwich {
     val bottom: Ingredient.Bread
@@ -36,6 +51,9 @@ sealed interface Sandwich {
         override val components: List<Ingredient.Component>
     ) : Sandwich {
         
+        operator fun plus(component: Ingredient.Component): Body =
+            copy(components = components + component)
+        
         class Builder {
             var bottom: Ingredient.Bread? = null
             var components: List<Ingredient.Component> = emptyList()
@@ -46,16 +64,14 @@ sealed interface Sandwich {
                 return Body(bottom!!, components)
             }
         }
-        
-        operator fun plus(component: Ingredient.Component): Body =
-            copy(components = components + component)
     }
     
     @ConsistentCopyVisibility
     data class Ready private constructor(
+        // Вместо наследования применяется композиция (Composition over inheritance)
         val body: Body,
         val top: Ingredient.Bread?
-    ) : Sandwich by body { // Вместо наследования применяется композиция (Composition over inheritance)
+    ) : Sandwich by body {
         
         class Builder {
             var body: Body? = null
@@ -70,20 +86,22 @@ sealed interface Sandwich {
 }
 
 /** `pure` for Sandwich.Body */
-fun sandwichBody(block: Sandwich.Body.Builder.() -> Unit): Sandwich.Body =
-    Sandwich.Body.Builder().apply { block () }.build()
+fun sandwichBody(block: Sandwich.Body.Builder.() -> Unit): Result<Sandwich.Body> =
+    Sandwich.Body.Builder().apply { block () }.build().let { Result.success(it) }
 
-/** `pure` for Sandwich.Ready */
 fun sandwichReady(block: Sandwich.Ready.Builder.() -> Unit): Sandwich.Ready =
     Sandwich.Ready.Builder().apply { block () }.build()
     
-/** bind/flatMap */
-infix fun Sandwich.Body.next(op: (Sandwich.Body) -> Sandwich.Body): Sandwich.Body =
-    op(this)
+/** bind/flatMap. Result<Body> -> Result<Body> */
+infix fun Result<Sandwich.Body>.next(op: (Result<Sandwich.Body>) -> Result<Sandwich.Body>): Result<Sandwich.Body> = 
+    if (isSuccess) op(this) else this
     
-/** bind/flatMap */
-infix fun Sandwich.Body.finish(op: (Sandwich.Body) -> Sandwich.Ready): Sandwich.Ready =
+/** bind/flatMap. Result<Body> -> Result<Ready> */
+infix fun Result<Sandwich.Body>.finish(op: (Result<Sandwich.Body>) -> Result<Sandwich.Ready>): Result<Sandwich.Ready> = 
     op(this)
+
+operator fun Result<Sandwich.Body>.plus(component: Ingredient.Component): Result<Sandwich.Body> =
+    mapCatching { it + component }
 
 /** Технология производства сэндвича */
 interface SandwichTechnology {
@@ -98,19 +116,27 @@ interface SandwichTechnology {
      */
     sealed interface Op {
         fun interface StartNewSandwich : Op {
-            operator fun invoke(bottom: Ingredient.Bread, component: Ingredient.Component): Sandwich.Body
+            operator fun invoke(bottom: Ingredient.Bread, component: Ingredient.Component): Result<Sandwich.Body>
         }
     
         fun interface AddComponent : Op {
-            operator fun invoke(sandwich: Sandwich.Body, component: Ingredient.Component): Sandwich.Body
+            operator fun invoke(sandwich: Result<Sandwich.Body>, component: Ingredient.Component): Result<Sandwich.Body>
         }
     
         fun interface FinishSandwich : Op {
-            operator fun invoke(sandwich: Sandwich.Body, top: Ingredient.Bread?): Sandwich.Ready
+            operator fun invoke(sandwich: Result<Sandwich.Body>, top: Ingredient.Bread?): Result<Sandwich.Ready>
         }
     }
 }
 
+/** Вспомогательный сервис */
+interface EventService {
+    fun send()
+    
+    interface MessageId {
+        
+    }
+}
 
 // ********************************
 // Layer 2. Domain & Business Logic
@@ -127,7 +153,7 @@ interface SandwichTechnology {
  * Скрипт (Use Case) на созданном в Layer 1 eDSL.
  * Бизнес-логика пишется только на абстракциях из Layer 1.
  */
-fun SandwichTechnology.myRecipe(): Sandwich.Ready = 
+fun SandwichTechnology.myRecipe(): Result<Sandwich.Ready> = 
     `start new sandwich`(Ingredient.Bread.TOAST, Ingredient.Component.TOMATO) next
     { `add component`(it, Ingredient.Component.CHEESE) } next
     { `add component`(it, Ingredient.Component.SALT) } finish 
@@ -140,6 +166,7 @@ fun SandwichTechnology.myRecipe(): Sandwich.Ready =
 
 // --------------------------------------
 // Модуль `impl`. Имплементации. Runtime.
+// --------------------------------------
 
 /**
  *  Autogenerated 
@@ -152,9 +179,9 @@ data class SandwichTechnologyImpl private constructor(
 ) : SandwichTechnology {
     
     class Builder {
-        var startNewSandwich: ((Ingredient.Bread, Ingredient.Component) -> Sandwich.Body)? = null
-        var addComponent: ((Sandwich.Body, Ingredient.Component) -> Sandwich.Body)? = null
-        var finishSandwich: ((Sandwich.Body, Ingredient.Bread?) -> Sandwich.Ready)? = null
+        var startNewSandwich: ((Ingredient.Bread, Ingredient.Component) -> Result<Sandwich.Body>)? = null
+        var addComponent: ((Result<Sandwich.Body>, Ingredient.Component) -> Result<Sandwich.Body>)? = null
+        var finishSandwich: ((Result<Sandwich.Body>, Ingredient.Bread?) -> Result<Sandwich.Ready>)? = null
         fun build(): SandwichTechnology {
             requireNotNull(startNewSandwich) { "`startNewSandwich` must be initialized!" }
             requireNotNull(addComponent) { "`addComponent` must be initialized!" }
@@ -167,9 +194,14 @@ data class SandwichTechnologyImpl private constructor(
 fun sandwichTechnology(block: SandwichTechnologyImpl.Builder.() -> Unit): SandwichTechnology =
     SandwichTechnologyImpl.Builder().apply { block() }.build()
 
+object IngredientsStorageImpl : IngredientsStorage {
+    override operator fun get(ingredient: Ingredient): Result<Ingredient> =
+        Result.success(ingredient)
+}
 
 // ----------------------
 // Модуль `app`. Runtime.
+// ----------------------
     
 /** Нормальная интерпретация */
 fun realInterpreter(): SandwichTechnology =
@@ -178,13 +210,22 @@ fun realInterpreter(): SandwichTechnology =
             sandwichBody {
                 bottom = bread
                 components += component
-            }
+            }.also { println("Op.StartNewSandwich processed.") }
         }
-        addComponent = { body, component -> body + component }
-        finishSandwich = { _body, _top -> 
-            sandwichReady {
-                body = _body
-                top = _top
+        addComponent = { body, component ->
+            //(body + component)
+            Result.failure<Sandwich.Body>(notFound(component))
+                .also { println("Op.AddComponent processed with error.") }
+        }
+        finishSandwich = { bodyResult, _top ->
+            /*Result.failure<Sandwich.Ready>(notFound(Ingredient.Bread.TOAST))
+                .also { println("Op.FinishSandwich processed with error.") }*/
+            
+            bodyResult.mapCatching { _body ->
+                sandwichReady {
+                    body = _body
+                    top = _top
+                }
             }
         }  
     }
@@ -198,10 +239,12 @@ fun crazyInterpreter(): SandwichTechnology =
             }
         }
         addComponent = { body, component -> body + component }
-        finishSandwich = { _body, _top -> 
-            sandwichReady {
-                body = _body
-                top = _top
+        finishSandwich = { bodyResult, _top ->
+            bodyResult.mapCatching { _body ->
+                sandwichReady {
+                    body = _body
+                    top = _top
+                }
             }
         }  
     }
@@ -210,4 +253,51 @@ fun main() {
     realInterpreter()
         .myRecipe()
         .also { println("sandwich: $it") }
+}
+
+// ************************************
+// External dependency copy-paste from
+// https://github.com/itarchdev/k3dm
+// ************************************
+
+/**
+ * Root of Formal Type System.
+ * */
+sealed interface Fts {
+    /**
+     * Вызывается в процессе создания объекта для его валидации.
+     *
+     * В случае неуспеха должен выкидывать исключение. Предполагается использование методов [require], [requireNotNull], и т.п.
+     *
+     * @throws IllegalStateException
+     * */
+    fun validate()
+}
+
+sealed interface ValueObject : Fts {
+    /** For `data class` */
+    interface Data : ValueObject {
+        fun <T: Data> fork(vararg args: Any?): T
+    }
+    
+    /** For `value class` */
+    interface Value<BOXED : Any> : ValueObject {
+        val boxed: BOXED
+        
+        /**
+         * Копирование объекта с новым значением.
+         *
+         * Имеет тот же смысл, что и метод `copy()` у `data class`. Обусловлен необходимостью создовать объект на
+         * уровне абстракции, чтобы иметь возможность писать логику, еще до генерации имплементации.
+         * */
+        public fun <T : Value<BOXED>> apply(boxed: BOXED): T
+    }
+    
+    /** For `enum class`, `sealed interface` */
+    public interface Sealed : ValueObject
+}
+
+public interface Entity : Fts {
+    public val id: ValueObject
+    public var content: ValueObject.Data
 }
